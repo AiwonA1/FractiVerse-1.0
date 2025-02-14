0e2456321d0924f4e6dbbea358b36b09cb31a424
Prudencio L. Mendez <espressolico@gmail.com>
2025-02-14 11:53:01 -0800

    Update fracticody_engine.py

--- fracticody_engine.py
+++ fracticody_engine.py
@@ -3,7 +3,7 @@
 Handles Recursive Intelligence, Fractal Cognition, and Decision-Making
 """
 from fracti_constants import FRACTICODY_VERSION
-from FractiCody1_0.core.fractal_cognition import FractalCognition
+from core.fractal_cognition import FractalCognition
 
 class FractiCodyEngine:
     def __init__(self):
