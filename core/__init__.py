a78f19de6251185d435ba1390c2631066e885511
Prudencio L. Mendez <espressolico@gmail.com>
2025-02-16 08:54:32 -0800

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,4 +1,4 @@
-# Ensures 'core' is treated as a package
+# Ensures 'core' is treated as a package and properly initializes modules
 from .fractal_cognition import FractalCognition
 from .memory_manager import MemoryManager
 from .fracti_fpu import FractiProcessingUnit
