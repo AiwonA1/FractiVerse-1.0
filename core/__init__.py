89f708775b1af6752ef3986f14f8ad5fbea9bfdf
Prudencio L. Mendez <espressolico@gmail.com>
2025-02-16 10:00:22 -0800

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,3 +1,4 @@
 from .fractal_cognition import FractalCognition
+from .fracti_decision_engine import FractiDecisionEngine
 from .memory_manager import MemoryManager
 from .fracti_fpu import FractiProcessingUnit
