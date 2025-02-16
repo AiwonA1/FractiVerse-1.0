26b209d23b711a8b1e0274d1db14fd5faeac86f1
Prudencio L. Mendez <espressolico@gmail.com>
2025-02-16 08:32:08 -0800

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,9 +1,9 @@
 import sys
 import os
 
-# Ensure core modules can be found
-sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+# Ensure core modules are found
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from .fractal_cognition import FractiCognition
-from .memory_manager import MemoryManager
-from .fracti_fpu import FractiProcessingUnit
+from core.fractal_cognition import FractiCognition
+from core.memory_manager import MemoryManager
+from core.fracti_fpu import FractiProcessingUnit
